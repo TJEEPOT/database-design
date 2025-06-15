895f5440ed54a193997251d77fe2a2ac727d70d2
Tjeepot <tjeepot@gmail.com>
2025-06-16 00:44:29 +0100

    fix create_secrets to add line breaks

--- _docker/create_secrets.py
+++ _docker/create_secrets.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Creates the secrets files for a demo test. Not to be used for production - see the secrets in github for prod access secrets.
+# Creates the secrets files for a demo test. Not to be used for production - see the secrets in github for prod access.
 
 def create_secrets():
-    with open("web-php.env", "w") as f: # db access .env file details.
-        f.write("MYSQL_DATABASE=canarytest")
-        f.write("MYSQL_USER=devtest")
-        f.write("MYSQL_PASSWORD=devtestpass1")
-        f.write("MYSQL_ROOT_PASSWORD=root")
+    with open("_docker/web-php.env", "w") as f: # db access .env file details.
+        f.write("MYSQL_DATABASE=canarytest\n")
+        f.write("MYSQL_USER=devtest\n")
+        f.write("MYSQL_PASSWORD=devtestpass1\n")
+        f.write("MYSQL_ROOT_PASSWORD=root\n")
 
 if __name__ == "__main__":
     create_secrets()
